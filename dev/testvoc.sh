681812ecd2757c42fcc54dff8d1c9b1d0fa6c5a1
Jim O'Regan <joregan@gmail.com>
2011-07-14 21:02:29 +0000

    don't want

--- dev/testvoc.sh
+++
@@ -1,5 +0,0 @@
-echo "==English->Italian============================";
-sh inconsistency.sh en-it > /tmp/en-it.testvoc; sh inconsistency-summary.sh /tmp/en-it.testvoc en-it
-echo ""
-echo "==Italian->English=============================";
-bash inconsistency.sh it-en > /tmp/it-en.testvoc; bash inconsistency-summary.sh /tmp/it-en.testvoc it-en
